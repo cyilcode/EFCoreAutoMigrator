0401dd4179170330fd1e68c4390daaf6095174a7
cyilcode <cmylmzbm@outlook.com>
2019-04-13 23:49:29 +0300

    Add secure data source checks

---
+++ EFCoreAutoMigrator/SecureDataSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EFCoreAutoMigrator
+{
+    public sealed class SecureDataSource
+    {
+        public SecureDataSource(string serverAddress, string databaseName)
+        {
+            if (string.IsNullOrEmpty(serverAddress))
+            {
+                throw new ArgumentException("Required parameter", nameof(serverAddress));
+            }
+
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentException("Required parameter", nameof(databaseName));
+            }
+
+            this.ServerAddress = serverAddress.ToUpperInvariant();
+            this.DatabaseName = databaseName.ToUpperInvariant();
+        }
+
+        public string ServerAddress { get; private set; }
+
+        public string DatabaseName { get; private set; }
+    }
+}
